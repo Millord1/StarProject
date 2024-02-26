fd7c2cda6dca04e471e9aa700c501b1c9d1125c8
Millord <roffat.theau@outlook.com>
2024-02-26 09:47:57 +0100

    add basic CRUD tests

--- app/Models/Star.php
+++ app/Models/Star.php
@@ -9,6 +9,42 @@ class Star extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'img_path',
+        'description',
+    ];
+
     protected $table = "stars";
 
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->first_name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->last_name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImgPath(): string
+    {
+        return $this->img_path;
+    }
 }
--- tests/Feature/StarTest.php
+++ tests/Feature/StarTest.php
@@ -10,6 +10,8 @@ class StarTest extends TestCase
 {
     use RefreshDatabase;
 
+    // Test CRUD of stars
+
     public function testCreateStars()
     {
         $stars = Star::factory()->count(5)->create();
@@ -19,6 +21,53 @@ class StarTest extends TestCase
         $this->assertDatabaseCount(Star::class, 5);
     }
 
+    public function testUpdateStars()
+    {
+        $firstName = 'Millord';
+        $lastName = 'Drollim';
+        $desc = 'A laravel developer';
 
+        Star::factory()->create([
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+            'img_path' => 'no_img',
+            'description' => $desc
+        ]);
+        $this->assertDatabaseCount(Star::class, 1);
+
+        // get last star from inmemory db
+        /** @var Star $star */
+        $star = Star::all()->last();
+        $this->assertDatabaseHas(Star::class, [
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+            'description' => $desc
+        ]);
+
+        // update star with a new description
+        $newDesc = 'A candidate';
+        $star->update([ 'description' => $newDesc ]);
+
+        // verify the previous 'description' is not existing anymore
+        $this->assertDatabaseMissing(Star::class, [ 'description' => $desc ]);
+        // verify the new one is saved
+        $this->assertDatabaseHas(Star::class, [ 'description' => $newDesc ]);
+    }
+
+    public function testDeleteStar()
+    {
+        Star::factory()->count(5)->create();
+        $this->assertDatabaseCount(Star::class, 5);
+
+        /** @var Star $star */
+        $star = Star::all()->last();
+        $star->delete();
+
+        $this->assertDatabaseMissing(Star::class, [
+            'first_name' => $star->getFirstName(),
+            'last_name' => $star->getLastName(),
+            'description' => $star->getDescription()
+        ]);
+    }
 
 }
